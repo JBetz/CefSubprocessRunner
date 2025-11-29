d26f22eae2f854f6bbc9740b3368c79238c34c53
Joe Betz <joebetz91@gmail.com>
2025-11-29 18:59:11 +0100

    remove unused delegate modules

--- src/client_app_delegates_common.cc
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2012 The Chromium Embedded Framework Authors. All rights
-// reserved. Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
-
-#include "process_handler.h"
-
-// static
-void ProcessHandler::RegisterCustomSchemes(CefRawPtr<CefSchemeRegistrar> registrar) {
-}
--- src/client_app_delegates_renderer.cc
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2012 The Chromium Embedded Framework Authors. All rights
-// reserved. Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
-
-#include "render_process_handler.h"
-
-// static
-void RenderProcessHandler::CreateDelegates(DelegateSet& delegates) {
-}
